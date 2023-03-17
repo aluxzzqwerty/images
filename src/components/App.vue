da6a004a67b899e3c31324bbfdde6d885b5ca5aa
aluxzzqwerty <aluanurmagambetova@mail.ru>
2023-03-17 19:05:41 +0600

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./AppHeader.vue";
+export default {
+  components: { AppHeader },
+  name: "App", 
+};
+</script>
+
+<style>
+</style>
